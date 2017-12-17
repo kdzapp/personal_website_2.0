02e48d8daa8d1b21a83529e683a5bcbe39217d04
kdzapp <kdzapp@umich.edu>
2017-12-17 16:48:42 -0500

    Updates, Actually going to finish this b****

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,10 +6,20 @@
           'Leader',
           'Innovator',
           'Dreamer'
-          ], i = 0;
+          ], i = 1;
       setInterval(function(){
-          $('#changingword').fadeOut(function(){
-              $(this).html(words[i=(i+1)%words.length]).fadeIn();
+          $('#changingword').fadeOut(function() {
+              $(this).html(words[i%words.length]).fadeIn();
+              if(words[i%words.length] == "Innovator") {
+                $('#changinga').fadeOut(function() {
+                  $(this).html("an*").fadeIn();
+                });
+              } else if(words[i%words.length] == "Dreamer") {
+                $('#changinga').fadeOut(function() {
+                  $(this).html("a").fadeIn();
+                });
+              }
+              i = i + 1;
           });
       }, 2500);
 
